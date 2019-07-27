b335baf32d75cc81d8803965127b19357a8c9e1f
eidalmanaseer <you@example.com>
2019-07-27 19:31:04 +0300

    drwoing1

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
-// start jq login
-$(document).ready(function() {
+//  start jq login
+ $(document).ready(function() {
 
-  var text=$("#username");
-  var pass=$(".abd2");
+   var text=$("#username");
+   var pass=$(".password");
 
-  text.on('keyup', function() {
+   text.on('keyup', function() {
 
 
-    if(text.val() === "admin" && pass.val() ==="admin") {
-      console.log('text = admin')
-      $("button").attr('hidden', false);
-    }
+     if(text.val() === "admin" && pass.val() ==="admin") {
+       console.log('text = admin')
+       $("button").attr('hidden', false);
+     }
+   });
+
+
+   pass.on('keyup', function() {
+
+     if(text.val() === "admin" && pass.val() ==="admin") {
+       console.log('text = admin')
+       $("button").attr('hidden', false);
+     }
+   });
+
   });
+ 
 
-
-  pass.on('keyup', function() {
-
-    if(text.val() === "admin" && pass.val() ==="admin") {
-      console.log('text = admin')
-      $("button").attr('hidden', false);
+  window.onload = function() {
+    var  color;
+    
+    colorArray = ['red', 'green', 'blue', 'orangered', 'yellow', 'rebeccapurple', 'tomato', 'orchid', 'goldenrod', 'mediumpurple', 'firebrick', 'green', 'fuchsia', 'gray', 'white', 'brown', 'peru', 'aqua', 'springgreen', 'slategrey', 'aquamarine', 'sienna', 'darkorange', 'darkcyan', 'violet']
+    
+    colorArray.forEach(function(element,index) {
+     $('#c'+(index+1)).click ( function(){   "use strict";
+        color = element;
+        console.log(color)
+      
+      })
+        
+    });
     }
-  });
-
-});
-// end jq login
-
-
